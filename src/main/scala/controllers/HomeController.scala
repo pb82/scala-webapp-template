718c77dc36656d3537d8bba20ac27f3329dcb8fe
Peter Braun <peter-braun@gmx.net>
2013-06-18 21:04:06 +0200

    added a simple .jsp and a favicon

--- src/main/scala/controllers/HomeController.scala
+++ src/main/scala/controllers/HomeController.scala
@@ -3,20 +3,16 @@ package controllers
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ResponseBody, RequestMapping}
 import org.springframework.ui.Model
+import org.springframework.web.servlet.ModelAndView
 
 /**
  * Created with IntelliJ IDEA.
- * User: peter
  * Date: 15.06.13
  * Time: 16:12
- * To change this template use File | Settings | File Templates.
  */
 
 @Controller
 class HomeController {
-  @ResponseBody
   @RequestMapping(Array("/"))
-  def home(): String = {
-    return "Hello from Scala & Spring"
-  }
+  def home(): String = "index"
 }
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,7 @@
+<!doctype html>
+<head>
+    <link href="/resources/images/scala.ico" rel="icon" type="image/x-icon" />
+</head>
+<body>
+    <h1>Hello from Spring & Scala</h1>
+</body>
